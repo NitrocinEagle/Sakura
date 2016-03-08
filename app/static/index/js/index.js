42c94ef28214ec6e5d5b12dec1306fccaa86ad69
pc-mist <mistsxd@gmail.com>
2016-03-08 15:37:18 +0700

    Menu and shop-info

--- app/static/index/js/index.js
+++ app/static/index/js/index.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
-    $('.my-slider').unslider();
+    if ($('.menu-nav>ul>li').hasClass('selected')) {
+        $('.selected').addClass('active');
+        var currentleft = $('.selected').position().left + "px";
+        var currentwidth = $('.selected').css('width');
+        $('.lamp').css({"left": currentleft, "width": currentwidth});
+    }
+    else {
+        $('.menu-nav>ul>li').first().addClass('active');
+        var currentleft = $('.active').position().left + "px";
+        var currentwidth = $('.active').css('width');
+        $('.lamp').css({"left": currentleft, "width": currentwidth});
+    }
+    $('.menu-nav>ul>li').hover(function () {
+        $('.menu-nav ul li').removeClass('active');
+        $(this).addClass('active');
+        var currentleft = $('.active').position().left + "px";
+        var currentwidth = $('.active').css('width');
+        $('.lamp').css({"left": currentleft, "width": currentwidth});
+    }, function () {
+        if ($('.menu-nav>ul>li').hasClass('selected')) {
+            $('.selected').addClass('active');
+            var currentleft = $('.selected').position().left + "px";
+            var currentwidth = $('.selected').css('width');
+            $('.lamp').css({"left": currentleft, "width": currentwidth});
+        }
+        else {
+            $('.menu-nav>ul>li').first().addClass('active');
+            var currentleft = $('.active').position().left + "px";
+            var currentwidth = $('.active').css('width');
+            $('.lamp').css({"left": currentleft, "width": currentwidth});
+        }
+    });
 });
